c10cc7cee60e0d7d4aa0d585f7ee47dc42e2429e
Paweł Piechnik <piechnik10@gmail.com>
2023-10-28 16:15:40 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Tailwindcss from "./Tailwindcss.svelte";
+</script>
+
+<Tailwindcss />
+<main>
+	<h1>Hello!</h1>
+</main>
+
+<style>
+	main {
+		padding: 0;
+		margin: 0;
+		min-width: 320px;
+		min-height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+</style>
